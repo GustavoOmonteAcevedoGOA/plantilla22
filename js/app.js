e704c978edd26daff269b28899329edaaaacee23
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-09-07 21:59:06 -0400

    second slider complete

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ $('.slider-one')
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next",
 });
+$('.slider-two')
+.not(".slick-intialized")
+.slick({
+    prevArrow:".site-slider-two .prev",
+    nextArrow:".site-slider-two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+});
